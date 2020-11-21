d790408ea15e6a36c4429e12821cefe4cd5519b7
SirPotatoIV <otoolej33@gmail.com>
2020-11-21 17:55:39 -0600

    Moved function for GitHub to Student Card. Added some minor styling to student cards.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import { getRepositories, getProfile } from './utils/github';
 import { students } from './data/student-data';
 import StudentCard from './components/StudentCard/StudentCard';
 
 function App() {
-  useEffect(() => {
-    // getRepositories(students[0]);
-    // getProfile(students[0]);
-  }, []);
-
   return (
     <div className="App">
       <h1>Class GitHub Summary</h1>
       {students.map((student) => (
-        <div key={student.username}>
+        <div key={student.username} className="card">
           <StudentCard student={student} />
         </div>
       ))}
--- src/components/StudentCard/StudentCard.tsx
+++ src/components/StudentCard/StudentCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Student } from '../../data/student-data.example';
+import { getRepositories, getProfile } from '../../utils/github';
 
 type StudentCardProps = {
   student: Student;
@@ -9,6 +10,7 @@ export default function StudentCard({ student }: StudentCardProps) {
   return (
     <>
       Name: {student.name} GitHub Username: {student.username}
+      <button onClick={() => getProfile(student)}>View Profile</button>
     </>
   );
 }
