b45b57190c844634d859e140e363c4beec38f8c3
SirPotatoIV <otoolej33@gmail.com>
2020-11-21 13:26:04 -0600

    Created file that will be used to store data for each student in the class and added it to the gitignore so private data will not appear in the repository.

---
+++ src/data/student-data.example.ts
@@ -0,0 +1,17 @@
+type Student = {
+  name: string;
+  username: string;
+};
+
+const students: Student[] = [
+  {
+    name: 'John Smith',
+    username: 'johnSmith1',
+  },
+  {
+    name: 'Jane Doe',
+    username: 'janeDoe1',
+  },
+];
+
+export { students };
