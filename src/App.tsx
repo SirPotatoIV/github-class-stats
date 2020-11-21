d9d19de32d95e1bfe89100c49857cd4badc32287
SirPotatoIV <otoolej33@gmail.com>
2020-11-21 16:41:28 -0600

    Created new component StudentCard and added it to App for testing.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import React, { useEffect } from 'react';
 import './App.css';
 import { getRepositories, getProfile } from './utils/github';
 import { students } from './data/student-data';
+import StudentCard from './components/StudentCard/StudentCard';
 
 function App() {
   useEffect(() => {
     // getRepositories(students[0]);
-    getProfile(students[0]);
+    // getProfile(students[0]);
   }, []);
-  // console.log(students[0]?.username);
+
   return (
     <div className="App">
       <h1>Class GitHub Summary</h1>
+      <StudentCard />
     </div>
   );
 }
