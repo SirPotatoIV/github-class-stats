a26778252fbb6b34334b186b8808cf699418aae0
Jake O'Toole <otoolej33@gmail.com>
2020-11-23 18:51:32 -0600

    Added student summary component and conditional formatting.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
-
 // components
 import StudentCard from './components/StudentCard/StudentCard';
-
+import StudentSummary from './components/StudentSummary/StudentSummary';
 // data and utils
 import { Student, students } from './data/student-data';
 import { getRepositories, getProfile } from './utils/github';
 
 function App() {
-  const [studentProfile, setStudentProfile] = useState({});
+  const [studentProfile, setStudentProfile] = useState<any>();
 
   // Function declared here so that response can be passed to other components
   const getStudent = (username: string): void => {
@@ -25,6 +24,7 @@ function App() {
           <StudentCard student={student} getStudent={getStudent} />
         </div>
       ))}
+      {studentProfile && <StudentSummary />}
     </div>
   );
 }
